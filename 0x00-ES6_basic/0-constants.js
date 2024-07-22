077fed17465965e8afd0821fecee67da43864fe9
Abdellahsyani <syaniabdos@gmail.com>
2024-07-22 19:35:07 +0100

    all

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,15 @@
+export function taskFirst() {
+  var task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  var combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
