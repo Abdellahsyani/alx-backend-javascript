2d8e9852108e557c763b70fb55381431e61d3795
Abdellahsyani <syaniabdos@gmail.com>
2024-07-22 20:24:09 +0100

    fix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,6 @@
-export default function taskBlock(trueOrFalse) {
+export default function taskBlock() {
   const task = false;
   const task2 = true;
-
-  if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
-  }
 
   return [task, task2];
 }
