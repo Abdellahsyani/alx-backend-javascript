8281cc95cf1aa154dcb97d36671039c36eb9b140
Abdellahsyani <syaniabdos@gmail.com>
2024-07-22 19:43:27 +0100

    correct

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,5 +1,5 @@
 export function taskFirst() {
-  var task = 'I prefer const when I can.';
+  const task = 'I prefer const when I can.';
   return task;
 }
 
@@ -8,7 +8,7 @@ export function getLast() {
 }
 
 export function taskNext() {
-  var combination = 'But sometimes let';
+  let combination = 'But sometimes let';
   combination += getLast();
 
   return combination;
