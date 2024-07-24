89f4f507da9e799adbc8670d39524cf16bb31cb2
Abdellahsyani <syaniabdos@gmail.com>
2024-07-24 23:07:28 +0100

    then corrected

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,8 +2,8 @@ export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
       return {
-      status: 200,
-      body: "success",
+        status: 200,
+        body: "success",
       }
     })
     .catch(() => {
