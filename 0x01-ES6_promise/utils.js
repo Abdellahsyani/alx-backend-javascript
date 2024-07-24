9fb5980a7e03f6b543a90c38ca369b2ef18219ff
Abdellahsyani <syaniabdos@gmail.com>
2024-07-24 19:10:07 +0100

    0-promise

---
+++ 0x01-ES6_promise/utils.js
@@ -0,0 +1,14 @@
+export function uploadPhoto() {
+  return Prmise.resolve({
+    status: 200,
+    body: 'photo-profile-1',
+  });
+}
+
+
+export function createUser() {
+  return Promise.resolve({
+    firstName: 'Guillaume',
+    lastName: 'Salva',
+  });
+}o
