0472c485d69d11cace037ab0e467e9b4831511ea
Abdellahsyani <syaniabdos@gmail.com>
2024-07-24 20:09:56 +0100

    1-promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,14 +1,12 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      const res = {
+      resolve({
         status: 200,
-	body: "Success",
-      };
-    resolve(res);
-    }
-    else {
-      reject(new Error("The fake API is not working currently"));
+	body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
