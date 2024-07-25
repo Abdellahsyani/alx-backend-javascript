10624b011b0b25f25b8b753c9297513f59a6213b
Abdellahsyani <syaniabdos@gmail.com>
2024-07-25 23:32:21 +0100

    divide

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,4 +1,4 @@
-export default function divideFunction(numeator, denominator) {
+export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {
     throw Error('cannot divide by 0');
   } else {
