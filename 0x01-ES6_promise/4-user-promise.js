830abae97c39e69d6f1376176051cf4d59ecdc7e
Abdellahsyani <syaniabdos@gmail.com>
2024-07-25 09:44:23 +0100

    promise

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,8 +1,3 @@
 export default function signUpUser(firstName, lastName) {
-  return new Promise((resolve, reject) => {
-      resolve({
-	firsName: firstName,
-	lastName: lastName,
-      });
-  });
+  return Promise.resolve({ firstName, lastName });
 }
