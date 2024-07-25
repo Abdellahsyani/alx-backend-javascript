5f103ae4ad60755844f16c2d4fe67ebf4d3cd054
Abdellahsyani <syaniabdos@gmail.com>
2024-07-25 16:19:56 +0100

    settled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,10 +4,10 @@ import uploadPhoto from './5-photo-reject';
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((val) => (
-      val.map((v) => ({
-	status: v.status,
-	value: v.status === 'fulfilled' ? v.value : String(v.reason),
+    .then((res) => (
+      res.map((o) => ({
+	status: o.status,
+	value: o.status === 'fulfilled' ? o.value : String(o.reason),
       }))
     ));
 }
