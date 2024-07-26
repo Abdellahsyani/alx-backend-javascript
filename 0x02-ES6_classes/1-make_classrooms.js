22df050224143134ce3d5b5ce4c4dc127b5e1a38
Abdellahsyani <syaniabdos@gmail.com>
2024-07-26 19:31:29 +0100

    corrcet

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,5 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  return [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34),
-  ];
+  return [19, 20, 34].map((size) => new ClassRoom(size));
 }
