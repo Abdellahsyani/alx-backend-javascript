911b1948b51949a8bb187e9683d7e81bbfaf669a
Abdellahsyani <syaniabdos@gmail.com>
2024-08-01 00:28:11 +0100

    corrected

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,8 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+  const buf = new DataView(new ArrayBuffer(length), 0, length);
+  buf.setInt8(position, value);
+  return buf;
+}
---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,16 @@
+export default function cleanSet(set, startString) {
+  const parts = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
+    }
+  }
+  return parts.join('-');
+}
